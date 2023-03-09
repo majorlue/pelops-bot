791cb7fdc57986100948b96564c082ed94afe66b
majorlue <majorrlue@gmail.com>
2023-03-08 18:32:37 -0700

    #patch updated startup logs

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -8,32 +8,33 @@ import {prisma} from '../handlers';
 
 const leadMonsters: string[] = [];
 
+// complete startup tasks, log time taken
 const onReady = async (client: Client) => {
   const clientId = client.user?.id;
   if (!clientId) throw Error('No client ID found!');
 
   const rest = new REST().setToken(config.BOT_TOKEN);
 
-  let loadTime = Date.now();
+  console.log(`Serving ${(await client.guilds.fetch()).size} servers`);
 
+  // register commands as global discord slash commands
+  let loadTime = Date.now();
   const commandData = commandList.map(command => command.data.toJSON());
   await rest.put(Routes.applicationCommands(clientId), {
     body: commandData,
   });
-
+  console.log(`Commands loaded: ${Object.keys(commandHash).join(', ')}`);
   console.log(
-    `Commands Loaded: ${Object.keys(commandHash).join(', ')}\n` +
-      `Loaded ${commandData.length} commands in ${Date.now() - loadTime}ms\n`
+    `Loaded ${commandData.length} commands in ${Date.now() - loadTime}ms`
   );
 
+  // retrieve encounters and load them as autocomplete suggestions
   loadTime = Date.now();
   (await prisma.encounter.findMany()).forEach(encounter =>
     leadMonsters.push(encounter.leader)
   );
-
   console.log(
     `Loaded ${leadMonsters.length} encounters in ${Date.now() - loadTime}ms`
   );
 };
-
 export {onReady, leadMonsters};
