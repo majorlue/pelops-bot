914c0cb2644add493af7033f5b7765d0fde4a291
majorlue <majorrlue@gmail.com>
2023-03-08 19:49:04 -0700

    #patch added discord status message

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,6 +1,6 @@
 import {REST} from '@discordjs/rest';
 import {Routes} from 'discord-api-types/v9';
-import {Client} from 'discord.js';
+import {ActivityType, Client} from 'discord.js';
 // Importing commandHash so it's loaded upon startup, rather than on the first command after startup
 import {commandHash, commandList} from '../commands';
 import {config} from '../config';
@@ -10,16 +10,13 @@ const leadMonsters: string[] = [];
 
 // complete startup tasks, log time taken
 const onReady = async (client: Client) => {
-  const clientId = client.user?.id;
-  if (!clientId) throw Error('No client ID found!');
+  if (!client.user) throw Error('Client not found');
+  const clientId = client.user.id;
+  const serverCount = (await client.guilds.fetch()).size;
 
   const rest = new REST().setToken(config.BOT_TOKEN);
 
-  logger.info(
-    `Serving ${(await client.guilds.fetch()).size} servers as ${
-      client.user?.tag
-    }`
-  );
+  logger.info(`Serving ${serverCount} servers as ${client.user?.tag}`);
 
   // register commands as global discord slash commands
   let loadTime = Date.now();
@@ -40,5 +37,12 @@ const onReady = async (client: Client) => {
   logger.info(
     `Loaded ${leadMonsters.length} encounters in ${Date.now() - loadTime}ms`
   );
+
+  client.user.setPresence({
+    activities: [
+      {type: ActivityType.Listening, name: `${serverCount} servers`},
+    ],
+  });
 };
+
 export {onReady, leadMonsters};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import {Client} from 'discord.js';
+import {ActivityType, Client} from 'discord.js';
 import {config} from './config';
 import {onInteraction, onReady} from './events';
+import {logger} from './handlers';
 
 // extracting required config vars
 const {BOT_TOKEN} = config;
@@ -12,12 +13,27 @@ const start = async () => {
   // run startup scripts
   client.on('ready', async () => await onReady(client));
 
+  // log server join and update discord presence
+  client.on('guildCreate', async guild => {
+    // retrieve new server count
+    const serverCount = (await client.guilds.fetch()).size;
+    logger.info(`Client joined guild #${serverCount}: ${guild.name}`);
+
+    if (client.user)
+      client.user.setPresence({
+        activities: [
+          {type: ActivityType.Listening, name: `${serverCount} servers`},
+        ],
+      });
+  });
+
   // handle user interactions (eg. commands)
   client.on(
     'interactionCreate',
     async interaction => await onInteraction(interaction)
   );
 
+  // client.user?.setPresence({activities: [{name: `Hello!`}]});
   await client.login(BOT_TOKEN);
 };
 
