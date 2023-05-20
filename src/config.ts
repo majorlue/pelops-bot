56e26c68dd3f392fc166414d2ad8cdc4a522dd9f
majorlue <majorrlue@gmail.com>
2023-05-20 09:48:08 -0600

    #none fixed submissin request message typo

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,12 @@ const envVars: Record<string, string | number | undefined> = {
     `Bug reports and suggestions welcomed in Discord!\n` +
     `/discord | v${version} | made by Major#1005`,
   CONTRIBUTION_REQUEST_MSG:
-    `\n\nIf you'd like to contribute frequently (or already have!), feeel free to contact Major#1005 to verify you as an official contributor. ` +
+    `\n\nIf you'd like to contribute frequently (or already have!), feel free to contact Major#1005 to verify you as an official contributor. ` +
     `This automatically updates Pelop's information for yourself and others, without delay. ` +
     `This isn't a requirement, though, you can still contribute normally via \`/submit\`. Thanks, traveler! Safe journey <3`,
   DISPLAY_CMD_DESC:
     `This message is updated hourly.` +
-    `\n\nIf you'd like to contribute frequently (or already have!), feeel free to contact Major#1005 to verify you as an official contributor. ` +
+    `\n\nIf you'd like to contribute frequently (or already have!), feel free to contact Major#1005 to verify you as an official contributor. ` +
     `This automatically updates Pelop's information for yourself and others, without delay. ` +
     `This isn't a requirement, though, you can still contribute normally via \`/submit\`. Thanks, traveler! Safe journey <3`,
   SUBMIT_THRESHOLD: process.env.SUBMIT_THRESHOLD || 3,
